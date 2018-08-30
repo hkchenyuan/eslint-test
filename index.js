48b03a4ef61fe9b889ce678a0f27fb59b1ab92b7
chenliyan <124699247@qq.com>
2018-08-30 17:19:58 +0800

    初始化项目

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * Created For:
+ * Author: chenLiYan.
+ * Date: 2018/8/30 15:32.
+ */
+(function () {
+    var a = 1;
+    // 初始
+    init();
+
+    function init() {
+
+        bindEvent();
+    }
+
+    // 事件绑定
+    function bindEvent() {
+
+    }
+})();
